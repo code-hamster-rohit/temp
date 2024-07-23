5ec78e5435b04a5aa5003193c5c3e94428b45c93
ROHIT KUMAR <21mc3013@rgipt.ac.in>
2024-07-23 21:41:19 +0530

    Delete static directory

--- static/js/index/partners.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const logosContainer = document.querySelector(".logos-container .logos");
-  const logos = logosContainer.innerHTML;
-  logosContainer.innerHTML += logos;
-});
